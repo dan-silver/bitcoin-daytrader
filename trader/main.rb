519ae99c702d3f10d12f9985e5ae03ca3ab72194
Dan Silver <das2c3@mail.missouri.edu>
2014-01-25 22:46:46 -0600

    Better UI

--- trader/main.rb
+++ trader/main.rb
@@ -16,7 +16,7 @@ class Trader
       config.client_id = ENV["BITSTAMP_CLIENT_ID"]
     end
     @transactionsDb = TransactionsDatabase.new
-    @transactionsDb.insert 0.61725807, 850, 2.49, :purchase
+    @transactionsDb.insert 0.61725807, 5000, 2.49, :purchase
     #@transactionsDb.insert 0.02, 900, 0.09, :sale
 
     @marketDb = MarketDatabase.new
@@ -32,6 +32,8 @@ class Trader
   def trade
     last_transaction = @transactionsDb.all_rows.last
     type = last_transaction[:type]
+    puts "Last Transaction:".green
+    puts last_transaction, ""
     type == "sale" ? consider_purchase(last_transaction) : consider_sale(last_transaction)
 
   end
@@ -40,7 +42,7 @@ class Trader
   #              [:timestamp, "DATETIME"]
 
   def consider_purchase(last_sale)
-    puts "consider_purchase!!!"
+    puts "Considering a purchase".green
     
     #fee_percent = 0.5
     current_market_data = @marketDb.all_rows.last
@@ -51,7 +53,10 @@ class Trader
     current_bitcoin_market_value = current_market_data[:btc_usd_buy]
 
     percent_change = (current_bitcoin_market_value-last_bitcoin_market_value)/last_bitcoin_market_value
-    puts percent_change
+    
+    puts "Current bitcoin price: $#{current_market_data[:btc_usd_buy].usd_round}"
+    puts "Waiting for a drop of #{@min_percent_drop*100}%"
+    puts "Percent change in bitcoin conversion value: " + "#{(percent_change*100).percent_round}%".cyan
     if percent_change < @min_percent_drop
       puts "Minimum purchase threshold reached"
       purchase current_bitcoin_market_value, usd_avail
@@ -69,7 +74,7 @@ class Trader
   end
 
   def consider_sale(last_purchase)
-    puts "Considering a sale"
+    puts "Considering a sale".green
 
     current_market_data = @marketDb.all_rows.last
 
@@ -82,7 +87,9 @@ class Trader
       puts "Minimum sale threshold reached"
       sell current_bitcoin_market_value, last_purchase[:btc]
     end
-    puts percent_change.percent_round
+    puts "Current bitcoin price: $#{current_market_data[:btc_usd_buy].usd_round}"
+    puts "Waiting for a gain of #{@min_percent_gain*100}%"
+    puts "Percent change in bitcoin conversion value: " + "#{percent_change.percent_round*100}%".cyan
   end
 
   def sell (btc_usd, btc_quantity)
@@ -96,7 +103,7 @@ end
 
 trader = Trader.new :percent_gain_for_sale => 0.01, :percent_change_for_purchase => -0.01
 while true do
-  puts "Running main loop..."
+  puts "",("*"*50).cyan,""
   trader.trade
 
   sleep 5
